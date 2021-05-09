95cd95f19d04bdfd9b4e256fbdc45314b627aded
Leonardo Garzón <l.garzonr@uniandes.edu.co>
2021-05-09 01:08:31 -0500

    first scenario set

--- Kraken/ghostTests/features/web/step_definitions/web_steps.rb
+++ Kraken/ghostTests/features/web/step_definitions/web_steps.rb
@@ -1,3 +1,13 @@
 if ENV["ADB_DEVICE_ARG"].nil?
   require 'kraken-mobile/steps/web/kraken_steps'
+
+  Then(/^I click on element with xpath "([^\"]*)"$/) do |selector|
+    @driver.find_element(:xpath, selector).click
+    sleep 2
+  end
+
+  Then(/^I check for "([^\"]*)" to exist in tag selector$/) do |selector|
+    $xpath= "//li[contains(@class, 'ember-power-select-option') and text() = '"+selector+"']"
+    puts @driver.find_element(:xpath, $xpath)
+  end
 end
