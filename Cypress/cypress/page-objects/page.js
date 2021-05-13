e20ec8cc4e0ecbd7fe55c488c7d1f374d36b2555
AlejandroFarfan <alejandrofarfanm@gmail.com>
2021-05-13 15:21:49 -0500

    Added ghost version folder to save screensshots.

--- Cypress/cypress/page-objects/page.js
+++ Cypress/cypress/page-objects/page.js
@@ -8,9 +8,10 @@ export class Page {
     backToPageList = 'a[href="#/pages/"]'
     pageEntry = '.gh-content-entry-title'
     postCardTitle = 'h2.post-card-title'
+    screenPath = Cypress.config('ghostUnderTest')+'/'
 
     goToPagesSection() {
-        cy.get('a[href*="#/pages/"]').click()
+        cy.get('a[href*="#/pages/"]').click({force: true})
     }
 
     getAndVisitUrl(selector) {
@@ -25,77 +26,78 @@ export class Page {
 
     createPageWithNoContent() {
         var step =0
-        cy.screenshot('scenario1_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario1_step'+step++).wait(1000)
         this.clickNewPage()
-        cy.screenshot('scenario1_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario1_step'+step++).wait(1000)
         cy.get(this.pageTitle).click()
-        cy.screenshot('scenario1_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario1_step'+step++).wait(1000)
         cy.get(this.backToPageList).first().click()
-        cy.screenshot('scenario1_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario1_step'+step++).wait(1000)
         cy.get(this.pageEntry).first().should('contain.text', '(Untitled)')
-        cy.screenshot('scenario1_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario1_step'+step++).wait(1000)
+
     }
 
     createPageUnpublished() {
         var step =0
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
         this.clickNewPage()
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
         cy.get(this.pageTitle).click()
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
         cy.get(this.backToPageList).first().click()
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
         cy.get(this.pageEntry).first().should("contain.text", '(Untitled)')
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
 
         cy.visit(this.publicSite)
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
         cy.get(this.postCardTitle).first().should('not.contain.text', '(Untitled)')
-        cy.screenshot('scenario2_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario2_step'+step++).wait(1000)
+
     }
 
     editFirstPage() {
         let title = faker.lorem.sentence(10)
         var step =0
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         this.getAndVisitUrl('a[title="Edit this post"]')
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get('textarea[placeholder="Page Title"]').clear().type(title)
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get('.koenig-editor__editor').type(faker.lorem.paragraph() + '{enter}').type(faker.lorem.paragraph())
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get('div.gh-publishmenu.ember-view').first().click()
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get('.gh-publishmenu-button').first().click()
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
 
         cy.get('.gh-notifications').should('be.visible')
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get(this.backToPageList).first().click()
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
         cy.get('.gh-list').should('contain.text', title)
-        cy.screenshot('scenario3_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario3_step'+step++).wait(1000)
 
     }
 
     deletePublishedPage(){
         var step =0
-        cy.screenshot('scenario4_step'+step++).wait(1000)
+        cy.screenshot(this.screenPath+'scenario4_step'+step++).wait(1000)
 
         cy.get('.gh-list-row')
         .then(($elements) => {
             const pages = $elements.length
-            cy.log('pages count ', pages)
             this.getAndVisitUrl('a[title="Edit this post"]')
-            cy.screenshot('scenario4_step'+step++).wait(1000)
+            cy.screenshot(this.screenPath+'scenario4_step'+step++).wait(1000)
             cy.get('.post-settings').click()
-            cy.screenshot('scenario4_step'+step++).wait(1000)
+            cy.screenshot(this.screenPath+'scenario4_step'+step++).wait(1000)
             cy.wait(1000)
             cy.get('form > .gh-btn > span').click().wait(1000)
-            cy.screenshot('scenario4_step'+step++).wait(1000)
+            cy.screenshot(this.screenPath+'scenario4_step'+step++).wait(1000)
             cy.get('.gh-btn-red').first().click()
             this.goToPagesSection()
-            cy.screenshot('scenario4_step'+step++).wait(1000)
+            cy.screenshot(this.screenPath+'scenario4_step'+step++).wait(1000)
             cy.get('.gh-list-row').should(($menuItems) => {
                 expect($menuItems).to.have.length(pages-1)
             })
