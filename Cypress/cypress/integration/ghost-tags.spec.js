cdffaeb9ffcc48ea1370ee74e1b74973ecc5a03f
Leonardo Garzón <l.garzonr@uniandes.edu.co>
2021-05-23 15:26:41 -0500

    tag form creation scenarios with random data

--- Cypress/cypress/integration/ghost-tags.spec.js
+++ Cypress/cypress/integration/ghost-tags.spec.js
@@ -6,89 +6,186 @@ import * as faker from "faker";
 describe('Tags E2E testing', () => {
     const login = new Login()
     const tagPage = new TagPage()
-    const post = new Post()
-    const url = Cypress.config('ghostUrl')
-    const screenPath = Cypress.config('ghostUnderTest') + '/tag-'
+    const scenarios = [
+        {
+            color: { type: 'hexaDecimal', length: 6, source: 'random' },
+            positive: false
+        },
+        {
+            slug: { type: 'slug', length: 2, source: 'random' },
+            positive: false
+        },
+        {
+            description: { type: 'words', length: 10, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', source: 'data-pool' },
+            positive: true
+        },
+        {
+            name: { type: 'words', source: 'dynamic-pool' },
+            color: { type: 'hexaDecimal', source: 'dynamic-pool' },
+            slug: { type: 'slug', source: 'dynamic-pool' },
+            description: { type: 'words', source: 'dynamic-pool' },
+            positive: true
+        },
+        {
+            name: { type: 'words',length:80, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'words', length: 1, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 5, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 7, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            slug: {  type: 'slug', length: 80, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            slug: {  type: 'words', length: 2, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            description: {  type: 'words', length: 80, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 2, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: true
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'words', length: 1, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 5, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 7, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            slug: {  type: 'slug', length: 80, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            slug: {  type: 'words', length: 2, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            description: {  type: 'words', length: 80, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            color: {  type: 'hexaDecimal', length: 6, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            positive: false
+        },
+        {
+            name: { type: 'words', length: 80, source: 'random' },
+            description: {  type: 'words', length: 10, source: 'random' },
+            slug: {  type: 'slug', length: 2, source: 'random' },
+            positive: false
+        },
+    ]
+
+    before(() => {
+        tagPage.initDataPools();
+        login.login(true)
+        Cypress.Cookies.defaults({
+            preserve: 'ghost-admin-api-session',
+          })
+    })
 
     beforeEach(() => {
-        login.login(true)
         tagPage.goToTagList()
-    })
-
-    it('New Tag should be visible on post settings', () => {
-        let step = 0;
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
         tagPage.clickOnNewTag();
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        const newTagName = faker.name.firstName()
-        tagPage.setTagName(newTagName);
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        tagPage.clickOnSaveTag();
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        cy.visit(url + "#/tags").wait(2000)
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        cy.get('.gh-nav-list-new a').first().click()
-        cy.wait(1000)
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        post.clickNewPost();
-        cy.screenshot(screenPath + 'scenario1_step' + step++).wait(1000)
-        post.clickPostSettings();
-        cy.screenshot(screenPath + 'scenario1_step' + step++, { capture: 'viewport' }).wait(1000)
-        post.checkTagOptionExist(newTagName)
-        cy.screenshot(screenPath + 'scenario1_step' + step++, { capture: 'viewport' }).wait(1000)
     })
 
-    it('Tag updates should be visible on webSite', () => {
-        let step = 0;
-        cy.screenshot(screenPath + 'scenario2_step' + step++).wait(1000);
-        const newTagName = faker.name.firstName();
-        const newTagDescription = faker.commerce.productDescription();
-        tagPage.clickOnTagWithPost()
-        cy.screenshot(screenPath + 'scenario2_step' + step++).wait(1000)
-        tagPage.setTagName(newTagName)
-        cy.screenshot(screenPath + 'scenario2_step' + step++).wait(1000)
-        tagPage.setTagDescription(newTagDescription)
-        cy.screenshot(screenPath + 'scenario2_step' + step++).wait(1000)
-        tagPage.getTagWeb().then(urlText => {
-            tagPage.clickOnSaveTag()
-            cy.screenshot(screenPath + 'scenario2_step' + step++).wait(1000)
-            cy.visit(urlText).wait(2000)
-            cy.screenshot(screenPath + 'scenario2_step' + step++, { capture: 'viewport' }).wait(1000)
-            tagPage.checkTagWebSiteTitle(newTagName)
-            tagPage.checkTagWebSiteDescription(newTagDescription)
+    scenarios.forEach((scenario, index) => {
+        it('tag form test ' + index, () => {
+            tagPage.fillForm(scenario);
+            tagPage.clickOnSaveTag();
+            tagPage.checkSaved(scenario.positive);
         })
     })
 
-    it('Deleted tag shoud not be on tagList', () => {
-        let step = 0;
-        cy.screenshot(screenPath + 'scenario3_step' + step++).wait(1000);
-        tagPage.clickOnTagWithNoPost();
-        cy.screenshot(screenPath + 'scenario3_step' + step++).wait(1000);
-        tagPage.getTagNameValue().then(tagName => {
-            tagPage.clickOnDeleteTag();
-            cy.screenshot(screenPath + 'scenario3_step' + step++).wait(1000);
-            tagPage.clickOnConfirmDelete();
-            cy.screenshot(screenPath + 'scenario3_step' + step++).wait(1000);
-            tagPage.goToTagList();
-            cy.screenshot(screenPath + 'scenario3_step' + step++).wait(1000);
-            tagPage.checkTagIsNotInList(tagName)
-        })
-    })
+    after(() => {
+        cy.clearCookie('ghost-admin-api-session')
+        cy.getCookies().should('be.empty')
+    });
 
-    it('internal tags should start by #', () => {
-        let step = 0;
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000);
-        tagPage.clickOnNewTag();
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000)
-        const newTagName = '#' + faker.name.firstName();
-        tagPage.setTagName(newTagName);
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000)
-        tagPage.clickOnSaveTag();
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000)
-        cy.visit(url + "#/tags").wait(2000)
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000)
-        tagPage.clickOnInternalTab();
-        cy.screenshot(screenPath + 'scenario4_step' + step++).wait(1000)
-        tagPage.checkTagOnList(newTagName)
-    })
 })
--- Cypress/cypress/page-objects/tag.js
+++ Cypress/cypress/page-objects/tag.js
@@ -1,13 +1,34 @@
+import * as faker from "faker";
+
 export class TagPage {
     inputName = 'input[name="name"]'
     inputDescription = 'textarea[name="description"]'
+    inputSlug = 'input[name="slug"]'
+    inputColor = 'input[name="accent-color"]'
     tagWebURL = '.ghost-url-preview'
     tagWebTitle = '.site-title'
     tatagWebDescription = '.site-description'
+    tagDataPool;
+    tagDynamicPool = [];
+    url = Cypress.config('ghostUrl')
+
+    initDataPools() {
+        cy.fixture('tag-data').then((data) => {
+            this.tagDataPool = data;
+        })
+
+        for (let i = 0; i < 100; i++) {
+            this.tagDynamicPool.push({
+                words: faker.lorem.words(2),
+                slug: faker.lorem.slug(2),
+                hexaDecimal: faker.datatype.hexaDecimal(6).slice(2)
+            })
+        }
+    }
 
     goToTagList() {
-        cy.get('.gh-nav-list li>a').contains('Tags').click()
-        cy.wait(1000)
+        cy.visit(this.url + "#/tags")
+        cy.wait(2000)
     }
 
     clickOnNewTag() {
@@ -32,11 +53,19 @@ export class TagPage {
         cy.get(this.inputDescription).clear({ force: true }).type(tagDescription, { force: true })
     }
 
+    setTagSlug(tagSlug) {
+        cy.get(this.inputSlug).clear({ force: true }).type(tagSlug, { force: true })
+    }
+
+    setTagColor(tagColor) {
+        cy.get(this.inputColor).clear({ force: true }).type(tagColor, { force: true })
+    }
+
     clickOnSaveTag() {
         cy.get('button>span').contains('Save').click()
     }
 
-    getTagWeb(){
+    getTagWeb() {
         return cy.get(this.tagWebURL).invoke('text')
     }
 
@@ -48,19 +77,19 @@ export class TagPage {
         cy.get(this.tatagWebDescription).contains(description).should('exist')
     }
 
-    getTagNameValue(){
+    getTagNameValue() {
         return cy.get(this.inputName).invoke('val')
     }
 
-    clickOnDeleteTag(){
+    clickOnDeleteTag() {
         cy.get('button>span').contains('Delete tag').click({ force: true }).wait(2000)
     }
 
-    clickOnConfirmDelete(){
+    clickOnConfirmDelete() {
         cy.get('.modal-footer>button>span').contains('Delete').click({ force: true })
     }
 
-    checkTagIsNotInList(tagName){
+    checkTagIsNotInList(tagName) {
         cy.get('h3.gh-tag-list-name').contains(tagName).should('not.exist')
     }
 
@@ -71,5 +100,48 @@ export class TagPage {
 
     checkTagOnList(name) {
         cy.get('h3.gh-tag-list-name').contains(name).should('exist')
+    }
+
+    checkSaved(positive) {
+        cy.get('button>span').contains('Saved').should(positive ? 'exist' : 'not.exist')
+        cy.get('button>span').contains('Retry').should(positive ? 'not.exist' : 'exist')
+    }
+
+    fillForm(config) {
+        Object.keys(config).forEach(formField => {
+            var value = this.getRandomValue(config[formField])
+            switch (formField) {
+                case 'name':
+                    this.setTagName(value)
+                    break;
+                case 'description':
+                    this.setTagDescription(value)
+                    break;
+                case 'slug':
+                    this.setTagSlug(value)
+                    break;
+                case 'color':
+                    this.setTagColor(value)
+                    break;
+            }
+            cy.wait(100)
+        })
+    }
+
+    getRandomValue(config) {
+        cy.log(config.type, config.source)
+        if (config.source == 'random')
+            switch (config.type) {
+                case 'words':
+                    return faker.lorem.words(config.length)
+                case 'slug':
+                    return faker.lorem.slug(config.length)
+                case 'hexaDecimal':
+                    return faker.datatype.hexaDecimal(config.length).slice(2)
+            }
+        if (config.source == 'data-pool')
+            return this.tagDataPool[Math.floor(Math.random() * 1001)][config.type]
+        if (config.source == 'dynamic-pool')
+            return this.tagDataPool[Math.floor(Math.random() * 1001)][config.type]
     }
 }
